8305d4c5aa5d0d9ad7375b9393eb5af974c36f62
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-10 12:52:48 +0000

    Chore(env): Update `.env.example` creator
    - Update `.env.example` creator script to handle multiple files
      such as `.env.local`, `env.development` etc.

--- scripts/create-env-example.sh
+++ scripts/create-env-example.sh
@@ -3,13 +3,15 @@
 # Creates an "*.example" file for the given ".env" by stripping out
 # The values and the comments.
 
-# This script is very barebones. It does not check whether the .env
-# file actually exists. It also doesn't check whether there are more than
-# one `.env` file of different flavors such as `.env.local`
-
-env_file=".env"
-
-awk -F'=' '{print $1}' $env_file | \
-  awk -F'#' '{print $1}' | \
-  awk NF > \
-  "$env_file.example"
+find . -type f \( \( \
+  -iname "*.env" \
+  -or -iname "*.env.*" \) \
+  -and -not -iname "*.example" \) | \
+    while read env_file;
+      do
+        echo "Handling $env_file"
+        awk -F'=' '{print $1}' $env_file | \
+          awk -F'#' '{print $1}' | \
+          awk NF > \
+          "$env_file.example"
+      done
