aa0e80b0c6a58c6453c8f859b28b27c4b2c3782c
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-10 13:59:20 +0000

    Feat(test): Update Echidna analysis script
    - Update script to be actually usable with this template repo. This was
      a legacy piece of code from the previous repo that lead to the
      development of this repo template. Now this code can be used to
      do fuzzing on the contracts that inherit this repo.
    - Update Main contract to include a numeric value to be used in fuzz
      testing purposes
    - Add a fuzz test for for the Main contract.

--- src/contracts/Main.sol
+++ src/contracts/Main.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.7;
 
 contract Main {
   string private greeting;
+  uint256 private counter = 0;
 
   constructor() {
     greeting = "Hello World!";
@@ -11,5 +12,13 @@ contract Main {
 
   function getGreeting() public view returns (string memory) {
     return greeting;
+  }
+
+  function incrementCounter() public {
+    counter += 1;
+  }
+
+  function getCounter() public view returns (uint256) {
+    return counter;
   }
 }
